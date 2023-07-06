4839d29504b2e8ab107acecb629c44e6d1b1c325
youwol-guillaume <greinisch@youwol.com>
2023-07-06 18:34:16 +0200

    :wrench: `jest.config.ts` => add `test-sequencer.js`

--- jest.config.ts
+++ jest.config.ts
@@ -3,5 +3,6 @@ import { Config } from 'jest'
 const jestConfig: Config = {
     preset: '@youwol/jest-preset',
     modulePathIgnorePatterns: [],
+    testSequencer: './src/tests/test-sequencer.js',
 }
 export default jestConfig
