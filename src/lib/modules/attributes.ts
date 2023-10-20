05b26f1b7e4c2682ab1f282e2288b701f8ab32ff
youwol-guillaume <greinisch@youwol.com>
2023-10-20 10:53:05 +0200

    📝 `Modules` => improve doc of `makeAttribute` & related functions

--- src/lib/modules/attributes.ts
+++ src/lib/modules/attributes.ts
@@ -2,8 +2,8 @@ import { OverrideType } from './traits'
 import { Configurations } from '..'
 
 /**
- *  A wrapper function for annotating a {@link Configurations.AttributeTrait} to be used as a module's
- *  configuration attribute.
+ *  A wrapper function for annotating a {@link Configurations.AttributeTrait} by {@link OverrideType} to be used
+ *  as a module's configuration attribute.
  *
  *  @param classFactory - The class of the actual {@link Configurations.AttributeTrait} to convert
  *  (e.g. {@link Configurations.Integer}).
@@ -12,10 +12,10 @@ import { Configurations } from '..'
  *  Use by default the annotation `overridable`.
  */
 export const makeAttribute = <
-    TR extends Configurations.AttributeTrait<unknown, TRunTime>,
+    TR extends Configurations.AttributeTrait<unknown, TOverride>,
     T extends new (p: unknown, mode: OverrideType) => TR,
     TParams extends ConstructorParameters<T>[0],
-    TRunTime extends OverrideType,
+    TOverride extends OverrideType,
 >(
     classFactory: T,
     params: TParams,
@@ -23,33 +23,38 @@ export const makeAttribute = <
 ) => new classFactory(params, options?.override || 'overridable')
 
 /**
- * A wrapper function for annotating a {@link Configurations.Integer} to be used as a module's configuration attribute.
+ * A wrapper function for annotating a {@link Configurations.Integer} by {@link OverrideType}  to be used as a
+ * module's configuration attribute.
  *
  * @param attrParams - The parameters forwarded as first constructor's parameter of {@link Configurations.Integer}.
  * @param annotations - (Optional) Additional options for annotating the attribute, for now the 'override' annotation.
  *  Use by default the annotation `overridable`.
  */
-export const integerAttribute = <TRunTime extends OverrideType = 'overridable'>(
+export const integerAttribute = <
+    TOverride extends OverrideType = 'overridable',
+>(
     attrParams: ConstructorParameters<typeof Configurations.Integer>[0],
-    annotations?: { override: TRunTime },
-): Configurations.Integer<TRunTime> =>
-    makeAttribute(Configurations.Integer<TRunTime>, attrParams, annotations)
+    annotations?: { override: TOverride },
+): Configurations.Integer<TOverride> =>
+    makeAttribute(Configurations.Integer<TOverride>, attrParams, annotations)
 
 /**
- * A wrapper function for annotating a {@link Configurations.Float} to be used as a module's configuration attribute.
+ * A wrapper function for annotating a {@link Configurations.Float} by {@link OverrideType} to be used as a
+ * module's configuration attribute.
  *
  * @param attrParams - The parameters forwarded as first constructor's parameter of {@link Configurations.Float}.
  * @param annotations - (Optional) Additional options for annotating the attribute, for now the 'override' annotation.
  *  Use by default the annotation `overridable`.
  */
-export const floatAttribute = <TRunTime extends OverrideType = 'overridable'>(
+export const floatAttribute = <TOverride extends OverrideType = 'overridable'>(
     attrParams: ConstructorParameters<typeof Configurations.Float>[0],
-    annotations?: { override: TRunTime },
-): Configurations.Float<TRunTime> =>
-    makeAttribute(Configurations.Float<TRunTime>, attrParams, annotations)
+    annotations?: { override: TOverride },
+): Configurations.Float<TOverride> =>
+    makeAttribute(Configurations.Float<TOverride>, attrParams, annotations)
 
 /**
- * A wrapper function for annotating a {@link Configurations.StringLiteral} to be used as a module's configuration
+ * A wrapper function for annotating a {@link Configurations.StringLiteral} by {@link OverrideType} to be used as
+ * a module's configuration
  * attribute.
  *
  * @param attrParams - The parameters forwarded as first constructor's parameter of
@@ -59,34 +64,36 @@ export const floatAttribute = <TRunTime extends OverrideType = 'overridable'>(
  */
 export const stringLiteralAttribute = <
     TLiteral,
-    TRunTime extends OverrideType = 'overridable',
+    TOverride extends OverrideType = 'overridable',
 >(
     attrParams: ConstructorParameters<
         typeof Configurations.StringLiteral<TLiteral>
     >[0],
-    annotations?: { override: TRunTime },
-): Configurations.StringLiteral<TLiteral, TRunTime> =>
+    annotations?: { override: TOverride },
+): Configurations.StringLiteral<TLiteral, TOverride> =>
     makeAttribute(
-        Configurations.StringLiteral<TLiteral, TRunTime>,
+        Configurations.StringLiteral<TLiteral, TOverride>,
         attrParams,
         annotations,
     )
 
 /**
- * A wrapper function for annotating a {@link Configurations.String} to be used as a module's configuration attribute.
+ * A wrapper function for annotating a {@link Configurations.String} by {@link OverrideType} to be used as a module's
+ * configuration attribute.
  *
  * @param attrParams - The parameters forwarded as first constructor's parameter of {@link Configurations.String}.
  * @param annotations - (Optional) Additional options for annotating the attribute, for now the 'override' annotation.
  *  Use by default the annotation `overridable`.
  */
-export const stringAttribute = <TRunTime extends OverrideType = 'overridable'>(
+export const stringAttribute = <TOverride extends OverrideType = 'overridable'>(
     attrParams: ConstructorParameters<typeof Configurations.String>[0],
-    annotations?: { override: TRunTime },
-): Configurations.String<TRunTime> =>
-    makeAttribute(Configurations.String<TRunTime>, attrParams, annotations)
+    annotations?: { override: TOverride },
+): Configurations.String<TOverride> =>
+    makeAttribute(Configurations.String<TOverride>, attrParams, annotations)
 
 /**
- * A wrapper function for annotating a {@link Configurations.JsCode} to be used as a module's configuration attribute.
+ * A wrapper function for annotating a {@link Configurations.JsCode} by {@link OverrideType} to be used as a
+ * module's configuration attribute.
  *
  * @param attrParams - The parameters forwarded as first constructor's parameter of {@link Configurations.JsCode}.
  * @param annotations - (Optional) Additional options for annotating the attribute, for now the 'override' annotation.
@@ -94,60 +101,66 @@ export const stringAttribute = <TRunTime extends OverrideType = 'overridable'>(
  */
 export const jsCodeAttribute = <
     TFct extends (...d: unknown[]) => unknown,
-    TRunTime extends OverrideType = 'overridable',
+    TOverride extends OverrideType = 'overridable',
 >(
     attrParams: ConstructorParameters<typeof Configurations.JsCode<TFct>>[0],
-    annotations?: { override: TRunTime },
-): Configurations.JsCode<TFct, TRunTime> =>
+    annotations?: { override: TOverride },
+): Configurations.JsCode<TFct, TOverride> =>
     makeAttribute(
-        Configurations.JsCode<TFct, TRunTime>,
+        Configurations.JsCode<TFct, TOverride>,
         attrParams,
         annotations,
     )
 
 /**
- * A wrapper function for annotating a {@link Configurations.Boolean} to be used as a module's configuration attribute.
+ * A wrapper function for annotating a {@link Configurations.Boolean} by {@link OverrideType} to be used as a
+ * module's configuration attribute.
  *
  * @param attrParams - The parameters forwarded as first constructor's parameter of {@link Configurations.Boolean}.
  * @param annotations - (Optional) Additional options for annotating the attribute, for now the 'override' annotation.
  *  Use by default the annotation `overridable`.
  */
-export const booleanAttribute = <TRunTime extends OverrideType = 'overridable'>(
+export const booleanAttribute = <
+    TOverride extends OverrideType = 'overridable',
+>(
     attrParams: ConstructorParameters<typeof Configurations.Boolean>[0],
-    annotations?: { override: TRunTime },
-): Configurations.Boolean<TRunTime> =>
-    makeAttribute(Configurations.Boolean<TRunTime>, attrParams, annotations)
+    annotations?: { override: TOverride },
+): Configurations.Boolean<TOverride> =>
+    makeAttribute(Configurations.Boolean<TOverride>, attrParams, annotations)
 
 /**
- * A wrapper function for annotating a {@link Configurations.AnyObject} to be used as a module's configuration attribute.
+ * A wrapper function for annotating a {@link Configurations.AnyObject} by {@link OverrideType}  to be used as a
+ * module's configuration attribute.
  *
  * @param attrParams - The parameters forwarded as first constructor's parameter of {@link Configurations.AnyObject}.
  * @param annotations - (Optional) Additional options for annotating the attribute, for now the 'override' annotation.
  *  Use by default the annotation `overridable`.
  */
 export const anyObjectAttribute = <
-    TRunTime extends OverrideType = 'overridable',
+    TOverride extends OverrideType = 'overridable',
 >(
     attrParams: ConstructorParameters<typeof Configurations.AnyObject>[0],
-    annotations?: { override: TRunTime },
-): Configurations.AnyObject<TRunTime> =>
-    makeAttribute(Configurations.AnyObject<TRunTime>, attrParams, annotations)
+    annotations?: { override: TOverride },
+): Configurations.AnyObject<TOverride> =>
+    makeAttribute(Configurations.AnyObject<TOverride>, attrParams, annotations)
 
 /**
- * A wrapper function for annotating a {@link Configurations.Any} to be used as a module's configuration attribute.
+ * A wrapper function for annotating a {@link Configurations.Any} by {@link OverrideType} to be used as a
+ * module's configuration attribute.
  *
  * @param attrParams - The parameters forwarded as first constructor's parameter of {@link Configurations.Any}.
  * @param annotations - (Optional) Additional options for annotating the attribute, for now the 'override' annotation.
  *  Use by default the annotation `overridable`.
  */
-export const anyAttribute = <TRunTime extends OverrideType = 'overridable'>(
+export const anyAttribute = <TOverride extends OverrideType = 'overridable'>(
     attrParams: ConstructorParameters<typeof Configurations.Any>[0],
-    annotations?: { override: TRunTime },
-): Configurations.Any<TRunTime> =>
-    makeAttribute(Configurations.Any<TRunTime>, attrParams, annotations)
+    annotations?: { override: TOverride },
+): Configurations.Any<TOverride> =>
+    makeAttribute(Configurations.Any<TOverride>, attrParams, annotations)
 
 /**
- * A wrapper function for annotating a {@link Configurations.CustomAttribute} to be used as a module's configuration attribute.
+ * A wrapper function for annotating a {@link Configurations.CustomAttribute} by {@link OverrideType} to be used as
+ * a module's configuration attribute.
  *
  * @param attrParams - The parameters forwarded as first constructor's parameter of
  * {@link Configurations.CustomAttribute}.
@@ -156,15 +169,15 @@ export const anyAttribute = <TRunTime extends OverrideType = 'overridable'>(
  */
 export const customAttribute = <
     TAttr,
-    TRunTime extends OverrideType = 'overridable',
+    TOverride extends OverrideType = 'overridable',
 >(
     attrParams: ConstructorParameters<
         typeof Configurations.CustomAttribute<TAttr>
     >[0],
-    annotations?: { override: TRunTime },
-): Configurations.CustomAttribute<TAttr, TRunTime> =>
+    annotations?: { override: TOverride },
+): Configurations.CustomAttribute<TAttr, TOverride> =>
     makeAttribute(
-        Configurations.CustomAttribute<TAttr, TRunTime>,
+        Configurations.CustomAttribute<TAttr, TOverride>,
         attrParams,
         annotations,
     )
