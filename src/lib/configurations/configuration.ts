b1df77a66e374d96f2e119734e67f72e1b6ca956
youwol-guillaume <greinisch@youwol.com>
2023-10-20 08:59:11 +0200

    ✨ `Configurations` =>  `Schema` & `AttributeTrait` can be annotated

--- src/lib/configurations/configuration.ts
+++ src/lib/configurations/configuration.ts
@@ -8,8 +8,11 @@ import { Immutable } from '../common'
  * Used in the definition of a module's {@link Configuration},
  * see {@link AttributeTrait} for the list of available attributes.
  */
-export type Schema = {
-    [k: string]: Schema | Schema[] | Attributes.AttributeTrait<unknown>
+export type Schema<TAnnotation = unknown> = {
+    [k: string]:
+        | Schema<TAnnotation>
+        | Schema<TAnnotation>[]
+        | Attributes.AttributeTrait<unknown, TAnnotation>
 }
 
 /** Helper to define the type of instantiated configuration from the schema type.
@@ -17,7 +20,10 @@ export type Schema = {
  * @typeParam TSchema The type of the schema associated to the configuration of the module.
  */
 export type ConfigInstance<TSchema> = {
-    [Property in keyof TSchema]: TSchema[Property] extends Attributes.AttributeTrait<unknown>
+    [Property in keyof TSchema]: TSchema[Property] extends Attributes.AttributeTrait<
+        unknown,
+        unknown
+    >
         ? ReturnType<TSchema[Property]['getValue']>
         : ConfigInstance<TSchema[Property]>
 }
@@ -58,7 +64,7 @@ export function extractConfigWith<T extends Schema>(
 
 function parseObject<TSchema extends Schema>(model: TSchema, values) {
     return Object.entries(model).reduce((acc, [k, v]) => {
-        const asAttribute = v as Attributes.AttributeTrait<unknown>
+        const asAttribute = v as Attributes.AttributeTrait<unknown, unknown>
         if ('getValue' in asAttribute) {
             return {
                 ...acc,
