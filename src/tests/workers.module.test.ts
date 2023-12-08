ceed5a525d1e47f601729e53ff9bc61b8b4bdd69
youwol-guillaume <greinisch@youwol.com>
2023-12-08 14:42:55 +0100

    ✅ remove `eslint-disable-next-line jest/no-done-callback`

--- src/tests/workers.module.test.ts
+++ src/tests/workers.module.test.ts
@@ -3,7 +3,7 @@ import {
     installTestWorkersEnvironment,
 } from './test.utils'
 import { ProjectState } from '../lib/project'
-import { from, of } from 'rxjs'
+import { firstValueFrom, from, of } from 'rxjs'
 import { mergeMap, tap } from 'rxjs/operators'
 
 beforeAll(async () => {
@@ -11,58 +11,54 @@ beforeAll(async () => {
     await installTestWorkersEnvironment()
 })
 
-/* eslint-disable-next-line jest/no-done-callback -- TODO: return Promise from subscription instead of using done() */
-test('one module', (done) => {
+test('one module', async () => {
     //const tb = project.getToolbox('@youwol/vs-flow-core/test-toolbox')
     //expect(tb.name).toBe('test-toolbox')
-    of(new ProjectState())
-        .pipe(
-            mergeMap((project) => {
-                return from(
-                    project.with({
-                        toolboxes: ['@youwol/vsf-rxjs', '@youwol/vsf-debug'],
-                        workersPools: [
-                            {
-                                id: 'A',
-                                startAt: 1,
-                                stretchTo: 1,
-                            },
-                        ],
-                        workflow: {
-                            branches: ['(of#of)>>(map#map)>>(console#log)'],
-                            configurations: {
-                                map: {
-                                    workersPoolId: 'A',
-                                    project: ({ context }) => {
-                                        return {
-                                            data: 2,
-                                            context,
-                                        }
-                                    },
+    const test$ = of(new ProjectState()).pipe(
+        mergeMap((project) => {
+            return from(
+                project.with({
+                    toolboxes: ['@youwol/vsf-rxjs', '@youwol/vsf-debug'],
+                    workersPools: [
+                        {
+                            id: 'A',
+                            startAt: 1,
+                            stretchTo: 1,
+                        },
+                    ],
+                    workflow: {
+                        branches: ['(of#of)>>(map#map)>>(console#log)'],
+                        configurations: {
+                            map: {
+                                workersPoolId: 'A',
+                                project: ({ context }) => {
+                                    return {
+                                        data: 2,
+                                        context,
+                                    }
                                 },
                             },
                         },
-                    }),
-                )
-            }),
-            tap((project: ProjectState) => {
-                const view = project.instancePool
-                    .inspector()
-                    .getModule('map')
-                    .html()
-                expect(view.innerText).toBeTruthy()
-            }),
-            mergeMap((project: ProjectState) => {
-                return project.instancePool.inspector().getModule('log')
-                    .inputSlots.input$.preparedMessage$
-            }),
-            tap(({ data }) => {
-                expect(data).toBe(2)
-            }),
-        )
-        .subscribe(() => {
-            done()
-        })
+                    },
+                }),
+            )
+        }),
+        tap((project: ProjectState) => {
+            const view = project.instancePool
+                .inspector()
+                .getModule('map')
+                .html()
+            expect(view.innerText).toBeTruthy()
+        }),
+        mergeMap((project: ProjectState) => {
+            return project.instancePool.inspector().getModule('log').inputSlots
+                .input$.preparedMessage$
+        }),
+        tap(({ data }) => {
+            expect(data).toBe(2)
+        }),
+    )
+    await firstValueFrom(test$)
 })
 
 test('error: worker pool does not exist', async () => {
